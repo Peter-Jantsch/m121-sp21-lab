7bc088c6680dfad6e762c9a185e3c358633156ce
Peter-Jantsch <75863835+Peter-Jantsch@users.noreply.github.com>
2021-01-23 13:35:23 -0600

    add lab 2

---
+++ lab02/tests/q36.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'q36',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> berkeley_markets.num_rows == 2
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> list(wheaton_markets.column('city')) == ['Wheaton', 'Wheaton']
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
